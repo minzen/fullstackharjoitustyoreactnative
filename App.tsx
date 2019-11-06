a9d95dcc412c3683a3f399d1b4ecefd08b0f7e5b
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2019-11-06 17:16:55 +0100

    Show a couple of buttons and text on the view with a couple of style changes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+import { ApolloProvider, Query } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        Lisätään vähän sisältöä ja katsotaan, mitä tapahtuu. Päivittyykö tämä
-        reaaliajassa? Yesh!
+      <Text style={styles.headerStyle}>Stored Notes</Text>
+      <Text style={styles.textStyle}>
+        This section will contain the actual notes obtained by GraphQL
       </Text>
+      <Button
+        title='Show notes'
+        onPress={() => {
+          console.log('Show notes pressed')
+          window.alert('Show notes')
+        }}
+      />
+      <Button
+        title='Login'
+        onPress={() => {
+          console.log('Login pressed')
+          window.alert('Login')
+        }}
+      />
     </View>
   )
 }
@@ -15,8 +33,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  headerStyle: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  textStyle: {
+    fontSize: 16
   }
 })
