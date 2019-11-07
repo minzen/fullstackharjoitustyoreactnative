24eb72ca93ad7ca256114ff6b03b994bb41d01bc
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2019-11-07 10:40:37 +0100

    Added dependencies for an easier implementation of the app: navigation with react-navigation, styling and components provided by react-native-elements. Started to create component based structure

---
+++ views/HomeScreen.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native-elements'
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  }
+  render() {
+    // const { navigate } = this.props.navigation
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => {
+          console.log('go to profile')
+          /*navigate('Profile', { name: 'Jane' })*/
+        }}
+      />
+    )
+  }
+}
+export default HomeScreen
