ba278815fd78fe3481151b3a9e37db143a4a60da
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2019-11-07 11:07:20 +0100

    Adds a simple search field for the notes display

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { gql } from 'apollo-boost'
 import { Query, Mutation } from 'react-apollo'
-import { Text } from 'react-native-elements'
+import { Text, SearchBar } from 'react-native-elements'
 
 const Notes = props => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleTextChange = search => {
+    console.log(`Setting ${search} to the search term`)
+    setSearchTerm(search)
+  }
+
   return (
     <>
+      <SearchBar
+        placeholder='Search for notes...'
+        onChangeText={handleTextChange}
+        value={searchTerm}
+      />
       <Text h2>Stored Notes</Text>
       <Text>
         This section will contain the actual notes obtained by GraphQL
